182aed1e50e42fa7ccd89a03a6355327b7c98761
Jeremiah <sentomerojeremy@gmail.com>
2026-06-08 14:06:41 +0300

    feat: add IntelliJ IDEA configuration files with device streaming settings and workspace preferences

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,18 +1,10 @@
-const path = require("path");
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
 const projectRoot = __dirname;
-const monorepoRoot = path.resolve(projectRoot, "../..");
 
 const config = getDefaultConfig(projectRoot);
 
-config.watchFolders = [monorepoRoot];
-config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, "node_modules"),
-  path.resolve(monorepoRoot, "node_modules"),
-];
-config.resolver.disableHierarchicalLookup = true;
 config.resolver.unstable_enablePackageExports = true;
 
 module.exports = withNativeWind(config, { input: "./global.css" });
